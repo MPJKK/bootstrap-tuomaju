e0332da69e5732a1fb40e7c4f32d8ba91866f3b1
Tuomas Juopperi <tuomaju@ety-a2123b-30.edu.wks.metropolia.fi>
2018-01-25 14:43:25 +0200

    värit ja rakenne

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-
-import { AppComponent } from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {AppComponent} from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './header/navbar/navbar.component';
+import { MainComponent } from './main/main.component';
+import { MainContentComponent } from './main/main-content/main-content.component';
+import { MainSidebarComponent } from './main/main-sidebar/main-sidebar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        NavbarComponent,
+        MainComponent,
+        MainContentComponent,
+        MainSidebarComponent,
+        FooterComponent
+    ],
+    imports: [
+        BrowserModule,
+        NgbModule.forRoot()
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
